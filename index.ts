640197af89c5b19e0c9588e8703e7dff295df8c1
Lakshya9545 <pandyalakshya04@gmail.com>
2025-02-04 01:04:48 +0530

    main file

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/authRoutes';
+import blogRoutes from "./routes/blogRoutes";
+import commentsRoutes from "./routes/commentsRoutes";
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(cors({
+  origin: process.env.CLIENT_URL, // Add this to your .env
+  credentials: true
+}));
+
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/blogs", blogRoutes);
+app.use("/api/comments", commentsRoutes);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`🚀 Server running on port ${process.env.PORT}`);
+});
